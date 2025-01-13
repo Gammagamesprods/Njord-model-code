869807f1b7f3e5fbb7575e35c4951d512ffaea06
someone <164253118+Gammagamesprods@users.noreply.github.com>
2025-01-12 19:57:24 -0500

    Update njord_moving_part_v0.1.1.ino

--- njord_moving_part_v0.1.1.ino
+++ njord_moving_part_v0.1.1.ino
@@ -1,12 +1,19 @@
+#include <Servo.h>
+Servo myservo;
 void setup() {
   // put your setup code here, to run once:
-  const float sver = 0.1;
+  const float sver = 0.5;
   const float hver = 0.2;
-  const float fver = 0.1;
+  const float fver = 0.3;
   pinMode(13,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(7,INPUT);
   pinMode(8,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(4,INPUT);
+  pinMode(12,OUTPUT);
+  myservo.attach(10);
   digitalWrite(13,LOW);
   Serial.begin(9600);
   Serial.println("init...");
@@ -34,16 +41,39 @@ void setup() {
   delay(300);
   digitalWrite(13,LOW);
   Serial.println("[INFO] Waiting for user key input");
+  digitalWrite(2,HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(8) == HIGH) {
     Serial.println("[INFO] Keyswitch confirmed. awaiting power switch.");
-    if (digitalRead(7) == HIGH) {
-      Serial.println("[INFO] Power received. starting mainprogram.ino...");
-      delay(2000);
-      
-    }
+    goto one;
+    one:
+      if (digitalRead(7) == HIGH) {
+        Serial.println("[INFO] Power received. starting mainprogram.ino...");
+        digitalWrite(2,LOW);
+        delay(1000);
+        digitalWrite(3,HIGH);
+        delay(2000);
+        goto two;
+      }
+       else {
+         goto one;
+       }
+        two:
+          if (digitalRead(4) == HIGH) {
+            Serial.println("[INFO] Switch triggered! running servo...");
+            myservo.write(50);
+            goto three;
+            three:
+              if (digitalRead(4) == LOW) {
+                myservo.write(-50);
+              }
+                else {
+                  goto three;
+                }
+              
+      }
   }
 }
